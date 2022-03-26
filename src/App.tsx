4cb249bbd95766d8ccb1d871772ffc1eb8aa5abb
Eryk <frontenddeveloperemd@gmail.com>
2022-03-26 07:01:20 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import { Router, Route } from "react-router-dom";
-import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import React, {Suspense,lazy} from "react";
 import './App.css';
-
 const Home = lazy(() => import("./components/home/home.component"));
 
+
+export function ApplicationLoader(){
+
+    return (
+      <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
+}
 function App() {
   return (
-    <div className="App">
-        {/* <Router history={history}>
-       <Route exact path="/" component={Home} />
-       </Router>  */}
-       <Home/>
+    <div className="App" >
+      <Router>
+      <Suspense fallback={<ApplicationLoader/>}>
+       <Routes>
+          <Route path="/" element={<Home/>} />
+
+      </Routes>
+      </Suspense>
+    </Router>
     </div>
   );
 }
--- src/components/home/home.component.tsx
+++ src/components/home/home.component.tsx
@@ -3,7 +3,7 @@ import React, { useRef, useCallback } from "react";
 
 export default function Home(){
     return(
-        <div>
+        <div style={{width:"50px", height:"100px", fontSize:"30px", color:"red"}}>
             Strona główna
         </div>
     )
